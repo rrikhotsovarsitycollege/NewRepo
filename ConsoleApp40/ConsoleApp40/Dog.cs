f4e348d416eb513e8cb73a1aa2ef014d276d62fa
SimonRRRR <RikhotsoSimon@gmail.com>
2026-05-09 09:45:26 +0200

    FirstPush
    fghf frfhggdhgfhhdfr   hjhf

---
+++ ConsoleApp40/ConsoleApp40/Dog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp40
+{
+    public class Dog
+    {
+        private string color;
+        private string breed;
+        private int age;
+
+        public Dog()
+        {
+            color = "red";
+            breed = "German";
+            age = 1;
+        }
+
+        public Dog(string cl, string br, int ag)
+        {
+            color = cl;
+            breed = br;
+            age = ag;
+        }
+
+        public void setColor(string cl)
+        {
+            color = cl;
+        }
+
+        public string getColor()
+        {
+            return color;
+        }
+
+        public void setBreed(string br)
+        {
+            breed = br;
+        }
+
+        public string getBreed()
+        {
+            return breed;
+        }
+
+        public void setAge(int ag)
+        {
+            age = ag;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+    }
+}
---
+++ ConsoleApp40/ConsoleApp40/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp40
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {         
+
+            Dog dog1;
+            dog1 = new Dog();
+            Console.WriteLine("Dog1 details...");
+            Console.WriteLine(dog1.getColor());
+            Console.WriteLine(dog1.getBreed());
+            Console.WriteLine(dog1.getAge());
+
+            string userColor;
+            Console.Write("Enter a color : ");
+            userColor = Console.ReadLine();
+
+            dog1.setColor(userColor);
+            Console.WriteLine("Dog1's new color...");
+            Console.WriteLine(dog1.getColor());
+
+            Dog dog2;
+            dog2 = new Dog();
+            Console.WriteLine("Dog2 details...");
+            Console.WriteLine(dog2.getColor());
+            Console.WriteLine(dog2.getBreed());
+            Console.WriteLine(dog2.getAge());
+
+            Dog dog3;
+            dog3 = new Dog("black", "Rotteweiller", 2);
+            Console.WriteLine("Dog3 details...");
+            Console.WriteLine(dog3.getColor());
+            Console.WriteLine(dog3.getBreed());
+            Console.WriteLine(dog3.getAge());
+        }
+
+    }
+}
